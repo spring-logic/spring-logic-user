c3080f27e4579b6f7f784f10d2d7d6dffd6e73f5
yangshinan <yangsn@figo.cn>
2017-04-10 11:48:29 +0800

    初始化spring boot项目

---
+++ src/main/java/cn/catwork/spring/logic/user/jpa/entity/Role.java
@@ -0,0 +1,21 @@
+package cn.catwork.spring.logic.user.jpa.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * Created by figo-007 on 2017/4/6.
+ */
+@Entity
+@Data
+public class Role {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String name;
+}
---
+++ src/main/java/com/fitcooker/app/FitcookerWebServiceApplication.java
@@ -0,0 +1,12 @@
+package com.fitcooker.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitcookerWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitcookerWebServiceApplication.class, args);
+	}
+}
