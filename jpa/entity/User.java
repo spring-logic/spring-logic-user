05ab4512764714c491c1626da14f5e27b62031d0
yangshinan <yangsn@figo.cn>
2017-04-11 15:34:44 +0800

    增加实体文件

---
+++ jpa/entity/User.java
@@ -0,0 +1,22 @@
+package cn.springlogic.user.jpa.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * Created by figo-007 on 2017/4/6.
+ */
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String username;
+    private String password;
+}
